3c843c2d1cb2b69ee2ebd3235a873918c407e762
Nelson Velasco <nelsonfvt@gmail.com>
2023-08-23 14:48:09 -0500

    23/08/2023

--- 3-Extraccion de caracteristicas/KSVD_example.py
+++ 3-Extraccion de caracteristicas/KSVD_example.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.linear_model import orthogonal_mp, OrthogonalMatchingPursuit
+from sklearn.cluster import KMeans
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from matplotlib.image import imread
 
@@ -46,8 +48,18 @@ for i in range(2, s_lenna[0], patch_size): #filas
     for j in range(2,s_lenna[1], patch_size): #columnas
         patch = lenna[i-1:i+2, j-1:j+2]
         patch = np.reshape(patch,(9, 1))
-        patch = patch / np.linalg.norm(patch)
+        #patch = patch / np.linalg.norm(patch)
         Do = np.c_[Do, patch] #append(Do, patch, axis=1)
+
+print('Tamaño del diccionario original:')
+print(Do.shape)
+#Eliminar linear dependencies del diccionario
+# se usa un algoritmo de agrupamiento
+k = Do.shape[0]*10 - 1#round(Do.shape[1]/50)
+kmeans = KMeans(k,max_iter=200,n_init='auto', random_state=0, init='k-means++').fit(Do.T)
+D1 = kmeans.cluster_centers_.T
+print('Tamaño diccionario reducido:')
+print(D1.shape)
 
 # Extrayendo algunos parches de Barbara 
 X = np.empty([9, 0]) #vacio
@@ -55,12 +67,17 @@ fila = round(s_barbe[1]/2)
 for i in range(2, s_barbe[1]-1, patch_size): #una fila
     patch = barbe[fila-1:fila +2, i-1:i+2]
     patch = np.reshape(patch, (9, 1))
-    patch = patch / np.linalg.norm(patch)
+    #patch = patch / np.linalg.norm(patch)
     X = np.c_[X, patch]
 
-print('Tamaño del diccionario:')
-print(Do.shape)
 print('Tamaño de X:')
 print(X.shape)
 
-W = OMP(Do, X[:, 0:1])
+W = OMP(D1, X[:, 1:2],tol=0.00000001)
+print('Pesos:')
+print(W)
+
+F = D1.dot(W)+227.4339
+print(F)
+print(X[:, 1:2])
+print(mean_squared_error(X[:, 0:1], F))
