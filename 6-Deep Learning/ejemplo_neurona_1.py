af671b5a8f15f601f42843a7195130a57b2b69b7
Nelson Velasco <nelsonfvt@gmail.com>
2023-09-13 15:51:01 -0500

    Add neuronas

--- 6-Deep Learning/ejemplo_neurona_1.py
+++ 6-Deep Learning/ejemplo_neurona_1.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-input = np.matrix([[0.0, 0.0],[0.0, 1.0],[1.0, 0.0],[1.0, 1.0]])
-output = np.array([0.0, 1.0, 1.0, 1.0])
 
 def neurona(inp, w):
     f = np.matmul(inp,w)
     #return np.tanh(f) #Tang hiperbolica
     return 1/(1 + np.exp(-f)) #sigmoide
+
+input = np.array([[0.0, 0.0],[0.0, 1.0],[1.0, 0.0],[1.0, 1.0]])
+output = np.array([0.0, 1.0, 1.0, 1.0])
 
 w = np.array([0.5, 0.5]) #Ajuste manualmente
 
