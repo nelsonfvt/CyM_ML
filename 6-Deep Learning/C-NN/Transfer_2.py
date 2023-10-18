19dbf38609486688aeafdc0fce739fc40b69292f
Nelson Velasco <nelsonfvt@gmail.com>
2023-10-18 12:52:26 -0500

    Added Transfer Learning Examples

---
+++ 6-Deep Learning/C-NN/Transfer_2.py
@@ -0,0 +1,159 @@
+# Tomado de: https://www.tensorflow.org/tutorials/images/transfer_learning
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+
+print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+
+# Descargando dataset (perros y gatos)
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
+                                                            shuffle=True,
+                                                            batch_size=BATCH_SIZE,
+                                                            image_size=IMG_SIZE)
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
+                                                                 shuffle=True,
+                                                                 batch_size=BATCH_SIZE,
+                                                                 image_size=IMG_SIZE)
+
+class_names = train_dataset.class_names
+
+# Visualizar algunas imagenes
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_dataset.take(1):
+  # for i in range(9):
+    # ax = plt.subplot(3, 3, i + 1)
+    # plt.imshow(images[i].numpy().astype("uint8"))
+    # plt.title(class_names[labels[i]])
+    # plt.axis("off")
+
+# Definiendo conjunto de test
+val_batches = tf.data.experimental.cardinality(validation_dataset)
+test_dataset = validation_dataset.take(val_batches // 5)
+validation_dataset = validation_dataset.skip(val_batches // 5)
+
+print('Number of validation batches: %d' % tf.data.experimental.cardinality(validation_dataset))
+print('Number of test batches: %d' % tf.data.experimental.cardinality(test_dataset))
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+
+
+# Aumento de datos
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip('horizontal'),
+  tf.keras.layers.RandomRotation(0.2),
+])
+
+# Visualizacion de aumento de datos
+# for image, _ in train_dataset.take(1):
+  # plt.figure(figsize=(10, 10))
+  # first_image = image[0]
+  # for i in range(9):
+    # ax = plt.subplot(3, 3, i + 1)
+    # augmented_image = data_augmentation(tf.expand_dims(first_image, 0))
+    # plt.imshow(augmented_image[0] / 255)
+    # plt.axis('off')
+
+# Descargando modelo pre-entrenado
+
+# Capa de re-escalado de pixeles (0. 255) -> (-1, 1)
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+
+# Creando modelo base a partir de modelo pre-entrenado
+IMG_SHAPE = IMG_SIZE + (3,)
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+
+# ESTRATEGIA: FINE TUNING
+# Congelando modelo base
+base_model.trainable = True
+base_model.summary()
+
+# Capa de clasificación
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+prediction_layer = tf.keras.layers.Dense(1)
+
+# Ensamble del modelo completo
+inputs = tf.keras.Input(shape=(160, 160, 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+# Numero total de capas del modelo base
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
+# Compilar modelo
+base_learning_rate = 0.0001
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer = tf.keras.optimizers.RMSprop(learning_rate=base_learning_rate/10),
+              metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0, name='accuracy')])
+
+
+model.summary()
+
+#fine_tune_epochs = 10
+#total_epochs =  initial_epochs + fine_tune_epochs
+total_epochs = 10
+
+history = model.fit(train_dataset,
+                         epochs=total_epochs,
+                         validation_data=validation_dataset)
+
+
+# Curvas de aprendizaje
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+#plt.show()
+plt.savefig(str('Fine_tuning_curves.jpg'), bbox_inches='tight')
+plt.clf()
